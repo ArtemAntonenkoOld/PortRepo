84eee49f09038f964ad0c1abd9a4377355eb38b8
GoGo228 <ghhfghhf5@gmail.com>
2016-08-30 22:19:33 +0300

    New Model for Auth User

---
+++ Portfolio.UIModels/ApiModels/MobileWebApiAuthorizedUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.UIModels.ApiModels
+{
+    public class MobileWebApiAuthorizedUser
+    {
+        public int UserId { get; set; }
+        public string UserPosition { get; set; }
+        public byte[] UserPhoto { get; set; }
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+        public int UserAge { get; set; }
+    }
+}
