ff5b60e1b7cf6a0692fa1ce7db18a71e2be6813d
GoGo228 <ghhfghhf5@gmail.com>
2016-09-21 16:39:46 +0300

    Auth branch

---
+++ Portfolio.BL/Mappers/ApiMappers/MobileWebApiUserWorkMapper.cs
@@ -0,0 +1,28 @@
+﻿using Portfolio.Entity;
+using Portfolio.UIModels.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Portfolio.BL.Mappers.BaseMappers;
+
+
+namespace Portfolio.BL.Mappers.ApiMappers
+{
+    public class MobileWebApiUserWorkMapper
+    {
+        public MobileWebApiUserWorkModel EntityToModel(tbWork model)
+        {
+            MobileWebApiUserWorkModel prop = new MobileWebApiUserWorkModel();
+            prop.IdUser = model.tbUser.cId;
+            prop.UserCompany = model.cCompany;
+            prop.CompanyDescription = model.cDescription;
+            prop.CompanyEndPeriod = model.cEndPeriod;
+            prop.CompanyStartPeriod = model.cStartPeriod; 
+
+
+            return prop;
+        }
+    }
+}
---
+++ Portfolio.UIModels/ApiModels/MobileWebApiUserWorkModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.UIModels.ApiModels
+{
+    public class MobileWebApiUserWorkModel
+    {
+        public int IdUser { get; set; }
+        public string UserCompany { get; set; }
+        public string UserStanding { get; set; }
+        public DateTime CompanyStartPeriod { get; set; }
+        public DateTime CompanyEndPeriod { get; set; }
+        public string CompanyDescription { get; set; }
+
+
+
+    }
+}
